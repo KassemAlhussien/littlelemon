c13d16d52bad4e3315f599fd26b349260bf3db3d
KASSEM HUSSEIN <kassemhussein.dev@gmail.com>
2024-06-02 21:34:19 +0300

    update app

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-## LITTLE LEMON PROJECT USING DJANGO
+# LITTLE LEMON PROJECT USING DJANGO
+
+## WHO RUN APP
+
+### Run Command 
+##
+      pip install -r requriemnt.txt
+##
+      python manage.py makemigrations
+##
+      python manage.py migrate
+##
+      python manage.py runserver
+
+## ENDPOINTS API
+### /menu  get all menu items 
+### /menu/{id} get menu item 
+### /reservations/ get all reservations  
+### /reservations/{id} get reserve
+### /auth-token/token/login get token by username and password
+
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,8 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("menu/", views.MenuItemsView.as_view()),
     path("menu/<int:pk>", views.MenuItemView.as_view()),
-    path('books/', views.BookingView.as_view()),
-    path('books/<int:pk>', views.BookingViewDetails.as_view()),
+    path('reservations/', views.BookingView.as_view()),
+    path('reservations/<int:pk>', views.BookingViewDetails.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth-token/', include('djoser.urls.authtoken')),
     path("api-token-auth/", obtain_auth_token),
